2d8c331b430a6c4149945629b90465df63335ece
EmmanuelSeujip <emmanuelseujip@gmail.com>
2026-06-29 20:21:21 +0100

    feat(card): Création et stylisation des cartes des restaurant

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './header/header';
+import { RestaurantList } from './restaurant-list/restaurant-list';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, RestaurantList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/restaurant-list/restaurant-card/restaurant-card.ts
+++ src/app/restaurant-list/restaurant-card/restaurant-card.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Restaurant } from '../../models/restaurant';
 
 @Component({
   selector: 'app-restaurant-card',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './restaurant-card.css',
 })
 export class RestaurantCard {
+  @Input() restaurant! : Restaurant;
+  @Output() ratingChanged = new EventEmitter<{ id: number; rating: number }>();
 
+  onStarClick(star: number): void {
+    this.ratingChanged.emit({ id: this.restaurant.id, rating: star });
+  }
+  stars = [0, 1, 2, 3, 4];
 }
