70490f913a5c768e5599b523f897dea74a8c50dc
EmmanuelSeujip <emmanuelseujip@gmail.com>
2026-06-29 20:59:20 +0100

    refractoring: Changement de la position du signal restaurant du composant restaurant-list pour le composant app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './header/header';
 import { RestaurantList } from './restaurant-list/restaurant-list';
+import { Restaurant } from './models/restaurant';
 
 
 @Component({
@@ -10,5 +11,13 @@ import { RestaurantList } from './restaurant-list/restaurant-list';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('delice');
+  allRestaurants: Restaurant[] = [
+    { id: 1, name: 'Le Calao Doré', district: 'Akwa', specialty: 'Ndolé aux crevettes', currentRating: 0 , url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_XuAVHlDWkaHihhczrbIeCj0CO3tbULUOIhNVc11PEg&s=10"},
+    { id: 2, name: 'Chez Madame Ngono', district: 'Bonapriso', specialty: 'Eru aux pieds de bœuf', currentRating: 0 , url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSV-uoBIhRx_bBfS_V3LejuN0XpA7V8e5N4-lF7bzKr73ERkCZRqIQrA_5F&s=10"},
+    { id: 3, name: 'La Fourchette Camerounaise', district: 'Bonanjo', specialty: 'Poulet DG', currentRating: 0 , url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRh9Du4puV5gFgDaP14rohTTgGSH1ByQd94-UZMJ2yUvw&s=10"},
+    { id: 4, name: 'Saveurs du Wouri', district: 'Bonamoussadi', specialty: 'Poisson braisé', currentRating: 0 , url:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2b/3e/bd/e5/restaurant-cosy-avec.jpg"},
+    { id: 5, name: "L'Akwa Gourmand", district: 'Akwa', specialty: 'Bobolo et sauce arachide', currentRating: 0 , url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdcBwOzk1t8qEQ6yG_ft4dKvncrGVxIKdGPOkN9okydG__QTWrRcUnPV0&s=10"},
+    { id: 6, name: 'Le Royal de Bali', district: 'Bali', specialty: 'Koki et plantain', currentRating: 0 , url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHjjEw0Bz8N8h0ujJ6u0pvXwAkXqKXTjdia61WI8VNSMLBJz_iHswGmj4&s=10"}
+  ]
+  restaurants = signal<Restaurant[]>(this.allRestaurants);
 }
--- src/app/restaurant-list/restaurant-list.ts
+++ src/app/restaurant-list/restaurant-list.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input, linkedSignal } from '@angular/core';
 import { Restaurant } from '../models/restaurant';
-import { signal } from '@angular/core';
 import { RestaurantCard } from './restaurant-card/restaurant-card';
 @Component({
   selector: 'app-restaurant-list',
@@ -9,14 +8,8 @@ import { RestaurantCard } from './restaurant-card/restaurant-card';
   styleUrl: './restaurant-list.css',
 })
 export class RestaurantList {
-  restaurants = signal<Restaurant[]>([
-    { id: 1, name: 'Le Calao Doré', district: 'Akwa', specialty: 'Ndolé aux crevettes', currentRating: 0 , url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_XuAVHlDWkaHihhczrbIeCj0CO3tbULUOIhNVc11PEg&s=10"},
-    { id: 2, name: 'Chez Madame Ngono', district: 'Bonapriso', specialty: 'Eru aux pieds de bœuf', currentRating: 0 , url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSV-uoBIhRx_bBfS_V3LejuN0XpA7V8e5N4-lF7bzKr73ERkCZRqIQrA_5F&s=10"},
-    { id: 3, name: 'La Fourchette Camerounaise', district: 'Bonanjo', specialty: 'Poulet DG', currentRating: 0 , url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRh9Du4puV5gFgDaP14rohTTgGSH1ByQd94-UZMJ2yUvw&s=10"},
-    { id: 4, name: 'Saveurs du Wouri', district: 'Bonamoussadi', specialty: 'Poisson braisé', currentRating: 0 , url:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2b/3e/bd/e5/restaurant-cosy-avec.jpg"},
-    { id: 5, name: "L'Akwa Gourmand", district: 'Akwa', specialty: 'Bobolo et sauce arachide', currentRating: 0 , url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdcBwOzk1t8qEQ6yG_ft4dKvncrGVxIKdGPOkN9okydG__QTWrRcUnPV0&s=10"},
-    { id: 6, name: 'Le Royal de Bali', district: 'Bali', specialty: 'Koki et plantain', currentRating: 0 , url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHjjEw0Bz8N8h0ujJ6u0pvXwAkXqKXTjdia61WI8VNSMLBJz_iHswGmj4&s=10"}
-  ])
+  allRestaurants = input<Restaurant[]>([]);
+  restaurants = linkedSignal(() => this.allRestaurants());
   updateRating(restaurantId: number, newRating: number): void {
     this.restaurants.update(restaurants =>
       restaurants.map(r =>
