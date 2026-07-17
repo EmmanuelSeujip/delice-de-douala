76aeccfe0c1a098fe78081c358e707e1b0dc5dac
EmmanuelSeujip <emmanuelseujip@gmail.com>
2026-07-17 11:09:31 +0100

    sauvegarde

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,20 @@ import { TemplateAdmin } from './pages/admin/template-admin/template-admin';
 import { Home } from './pages/user/home/home';
 import { PlatsList } from './pages/user/plats-list/plats-list';
 import { Commande } from './pages/user/commande/commande';
+import { UserManagement } from './pages/admin/user-management/user-management';
 
 export const routes: Routes = [
-  {path: '',component:Home},
-  { path: 'admin', component: TemplateAdmin },
-  {path:'plat', component:PlatsList},
-  {path:'commande', component:Commande}
+  { path: '', component: Home },
+  { path: 'admin', 
+    component: TemplateAdmin ,
+    children: [
+      {
+        path:'user',
+        component:UserManagement
+      }
+    ]
+      
+  },
+  { path: 'plat', component: PlatsList },
+  { path: 'commande', component: Commande }
 ];
--- src/app/pages/admin/template-admin/template-admin.ts
+++ src/app/pages/admin/template-admin/template-admin.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Sidebar } from "../../../components/adminTemplate/sidebar/sidebar";
 import { HeaderAdmin } from "../../../components/adminTemplate/header-admin/header-admin";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-template-admin',
-  imports: [Sidebar, HeaderAdmin],
+  imports: [Sidebar, HeaderAdmin, RouterOutlet],
   templateUrl: './template-admin.html',
   styleUrl: './template-admin.css',
 })
